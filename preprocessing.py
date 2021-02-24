c263e3e8c12e911d0007b0a20b80aff51c71a1ed
hamburgersct <hamburgersct@gmail.com>
2021-02-24 16:15:41 +0800

    add preprocessing module

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,14 @@
 import os
 import csv
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+# from autocorrect import spell
 
 path = "./passages/"
 directories = os.listdir(path)
+
 
 # print(directories)
 
@@ -15,18 +21,40 @@ directories = os.listdir(path)
 #             stu_num = filename[:9] + '.csv'
 #             os.rename(os.path.join(full_dir,filename),os.path.join(full_dir,stu_num))
 
+def text_cleaner(text):
+    rules = [
+        {r'>\s+': u'>'},  # remove spaces after a tag opens or closes
+        {r'\s+': u' '},  # replace consecutive spaces
+        {r'\s*<br\s*/?>\s*': u'\n'},  # newline after a <br>
+        {r'</(div)\s*>\s*': u'\n'},  # newline after </p> and </div> and <h1/>...
+        {r'</(p|h\d)\s*>\s*': u'\n\n'},  # newline after </p> and </div> and <h1/>...
+        {r'<head>.*<\s*(/head|body)[^>]*>': u''},  # remove <head> to </head>
+        {r'<a\s+href="([^"]+)"[^>]*>.*</a>': r'\1'},  # show links instead of texts
+        {r'[ \t]*<[^<]*?/?>': u''},  # remove remaining tags
+        {r'^\s+': u''},  # remove spaces at the beginning
+        {r'[^\w\s]': u' '}  #remove punctuation
+    ]
+    for rule in rules:
+        for (k, v) in rule.items():
+            regex = re.compile(k)
+            text = regex.sub(v, text)
+    text = text.rstrip()
+    return text.lower()
 
 
-def preprocessing(src_top_dir,dist_top_dir):
+def preprocessing(src_top_dir, dist_top_dir):
     '''
     :param top_dir: The directory of texts. in this project, it is './passage'
     :return:
     '''
+    stop_words = set(stopwords.words('english'))
+    wnl = WordNetLemmatizer()
+
     for dir in os.listdir(src_top_dir):
-        dir_path = os.path.join(src_top_dir,dir)
+        dir_path = os.path.join(src_top_dir, dir)
         filenames = os.listdir(dir_path)
         for file in filenames:
-            with open(os.path.join(dir_path,file), encoding='utf-8-sig') as csvFile:
+            with open(os.path.join(dir_path, file), encoding='utf-8-sig') as csvFile:
                 reader = csv.reader(csvFile)
                 passage_list = []
                 for row in reader:
@@ -34,20 +62,23 @@ def preprocessing(src_top_dir,dist_top_dir):
                 passage = ' '.join(passage_list)
 
                 # TODO:preprocess texts
+                passage_clean = text_cleaner(passage)
+                tokens = word_tokenize(passage_clean)
+                filtered_sentence = [w for w in tokens if not w in stop_words]
+                lem_sentence = [wnl.lemmatize(w) for w in filtered_sentence]
+                print(len(lem_sentence))
+                # dist_passage = ' '.join(lem_sentence)
 
-            dist_dir = os.path.join(dist_top_dir,dir)
-            if not os.path.exists(dist_dir):
-                os.makedirs(dist_dir)
-            with open(os.path.join(dist_dir,file[:-4]+'.txt'), 'w') as txtFile:
-                txtFile.write(passage)
-                txtFile.close()
-            csvFile.close()
+            # dist_dir = os.path.join(dist_top_dir, dir)
+            # if not os.path.exists(dist_dir):
+            #     os.makedirs(dist_dir)
+            # with open(os.path.join(dist_dir, file[:-4] + '.txt'), 'w') as txtFile:
+            #     txtFile.write(passage)
+            #     txtFile.close()
+            # csvFile.close()
 
 
 if __name__ == '__main__':
-    preprocessing('./passages','./passages_processed')
-
-
+    preprocessing('./passages', './passages_processed')
 
 # TODO：analyse statistic characteristics
-
