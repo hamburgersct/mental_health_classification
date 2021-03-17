bbf1526d175509cfdc660f09f1e01c6bb492103b
hamburgersct <hamburgersct@gmail.com>
2021-03-17 16:08:13 +0800

    update svm classifier with student metadata

--- svm.py
+++ svm.py
@@ -1,13 +1,14 @@
 # coding=gbk
 import pandas as pd
 import numpy as np
-import scipy.sparse as sp
+from imblearn.over_sampling import SMOTE
 from sklearn.linear_model import SGDClassifier
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import FunctionTransformer
 
 def standard_svm():
     text_train = pd.read_csv('./frac=0.8/training_set_0.8.csv')
@@ -24,11 +25,15 @@ def standard_svm():
                          ])
     parameters = {
         'vect__ngram_range': [(1,2), (1,3)],
-        'tfidf__use_idf': (True, False),
         'clf__alpha': (1e-4, 1e-6)
     }
 
-    gs_clf = GridSearchCV(pipeline, parameters, n_jobs=1, verbose=1)
+    # Training config
+    kfold = StratifiedKFold(n_splits=5)
+    scoring = {'Accuracy': 'accuracy', 'F1': 'f1_macro'}
+    refit = 'F1'
+
+    gs_clf = GridSearchCV(pipeline, parameters, n_jobs=1, verbose=1, cv=kfold, scoring=scoring, refit=refit)
     gs_clf.fit(X_train, y_train)
 
     predicted = gs_clf.predict(X_test)
@@ -37,37 +42,53 @@ def standard_svm():
     print(metrics.confusion_matrix(y_test, predicted))
     print(metrics.f1_score(y_test, predicted, average='weighted'))
 
-def metadata_svm():
+
+# Create Function Transformer to use Feature Union
+def get_numeric_data(x):
+    return np.array(x.iloc[:, 26:-2])
+
+def get_text_data(x):
+    return x['text'].tolist()
+
+def metadata_svm_fu():
     text_train = pd.read_csv('./frac=0.8/training_set_0.8.csv')
     text_test = pd.read_csv('./frac=0.8/testing_set_0.8.csv')
 
-    X_train = text_train['text'].tolist()
-    X_test = text_test['text'].tolist()
     y_train = text_train['可能问题'].tolist()
     y_test = text_test['可能问题'].tolist()
 
-    vectorizer_x = TfidfVectorizer()
-    X_train = vectorizer_x.fit_transform(X_train).toarray()
-    X_test = vectorizer_x.transform(X_test).toarray()
-    print("tf-idf with", str(np.array(X_train).shape))
+    transformer_numberic = FunctionTransformer(get_numeric_data)
+    transformer_text = FunctionTransformer(get_text_data)
 
-    metadata_train = np.array(text_train.iloc[:, 26:-2])
-    metadata_test = np.array(text_test.iloc[:, 26:-2])
+    # Create a pipeline to concatenate Tfidf Vector and Numeric data
+    # Use SVM as classifier
+    pipeline = Pipeline([
+        ('metadata', FeatureUnion([
+            ('numeric_feature', Pipeline([
+                ('selector', transformer_numberic)
+            ])),
+            ('text_features', Pipeline([
+                ('selector', transformer_text),
+                ('vec', TfidfVectorizer(ngram_range=(1,3)))
+            ]))
+        ])),
+        ('clf', SGDClassifier())
+    ])
 
-    print(np.array(X_train).shape, metadata_train.shape)
-
-    combine_train = np.concatenate((X_train, metadata_train), axis=1)
-    combine_test = np.concatenate((X_test, metadata_test), axis=1)
-
-    pipeline = Pipeline([('clf', SGDClassifier())])
+    # Grid Search Parameters for SGDClassifer
     parameters = {
         'clf__alpha': (1e-4, 1e-6)
     }
 
-    gs_clf = GridSearchCV(pipeline, parameters, n_jobs=1, verbose=1)
-    gs_clf.fit(combine_train, y_train)
+    # Training config
+    kfold = StratifiedKFold(n_splits=5)
+    scoring = {'Accuracy': 'accuracy', 'F1': 'f1_macro'}
+    refit = 'F1'
 
-    predicted = gs_clf.predict(combine_test)
+    gs_clf = GridSearchCV(pipeline, parameters, n_jobs=1, verbose=1, cv=kfold, scoring=scoring, refit=refit)
+    gs_clf.fit(text_train, y_train)
+
+    predicted = gs_clf.predict(text_test)
 
     print(metrics.classification_report(y_test, predicted))
     print(metrics.confusion_matrix(y_test, predicted))
@@ -75,4 +96,6 @@ def metadata_svm():
 
 
 if __name__ == '__main__':
-    metadata_svm()
+    # metadata_svm_fu()
+    # standard_svm()
+    oversamp_svm_fu()
